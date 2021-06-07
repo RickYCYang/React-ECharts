2b89271c95212e162ca05f2480998713d93ab836
RickYCYang <rickyang2910@gmail.com>
2021-06-07 23:35:44 +0800

    TPMP 斑馬圖 Study

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import EChart from './Component/EChart';
 
 function App() {
+
+  const style = {
+    height: '100vh',
+    width: '100vw',
+    padding: '50px',
+    boxSizing: 'border-box'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style = {style}>
+      <EChart/>
     </div>
   );
 }
